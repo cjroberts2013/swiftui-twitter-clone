f66262b939fe3b7044c17617ab301c31ddf9c623
Charles Roberts <cjroberts2013@gmail.com>
2022-05-08 21:59:55 -0400

    Initial Commit

---
+++ swiftui-twitter-clone/swiftui_twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_twitter_cloneApp.swift
+//  swiftui-twitter-clone
+//
+//  Created by Charles Roberts on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
